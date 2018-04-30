e9a9f4050dc969efef76bd0b62b0a805f3d4138b
Tony White <vshierdom@gmail.com>
2018-04-30 16:32:06 -0700

    Add dropbox, remove ramping, tweak default comments

--- tanktester.py
+++ tanktester.py
@@ -6,6 +6,12 @@ import time
 import datetime
 import pigpio
 import sys
+import dropbox
+from dropbox.files import WriteMode
+from dropbox.exceptions import ApiError, AuthError
+TOKEN = ''
+LOCALFILE = '%s'fname #file to backup
+BACKUPPATH = '/Tankinator_logs/%s'
 pi = pigpio.pi() #initialize servo style ESC control
 MIN_THROTTLE = 1000
 MAX_THROTTLE = 2000
@@ -34,7 +40,7 @@ print "The current reading of the force sensor is %s"%scale_level
 test_name = raw_input('Enter name of this test: ')
 timenow='{date:%Y-%m-%d %H:%M:%S}.csv'.format( date=datetime.datetime.now() )
 fname = test_name + ' ' + timenow #create filename from user name and time at start of test
-d_steps = input('Duration of each throttle step (seconds) (Adam says 5sec, with 30 second pause between)')
+d_steps = input('Duration of each throttle step (seconds) (Adam says 3sec, with 20 second pause between)')
 num_steps = input('# of steps from 0 to full throttle (one direction - if wrong, swap 2 motor wires)')
 settle_time = 20 #seconds between throttle steps. Change to input if frequently varied across tests
 loop_period = 0.1 # 1/ this = hz
@@ -108,4 +114,40 @@ while (throttle <=MAX_THROTTLE) and (prevthrottle != MAX_THROTTLE): # exit when 
 pi.set_servo_pulsewidth(18,MIN_THROTTLE) #turn onff motor post test
 time.sleep(2)
 print "test complete dude"
- # relaunches this script for next test
+# Add OAuth2 access token here.
+# You can generate one for yourself in the App Console.
+# See <https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/>
+def backup():# Uploads contents of logfile to Dropbox
+    with open(LOCALFILE, 'rb') as f:
+        # We use WriteMode=overwrite to make sure that the settings in the file
+        # are changed on upload
+        print("Uploading " + LOCALFILE + " to Dropbox as " + BACKUPPATH + "...")
+        try:
+            dbx.files_upload(f.read(), BACKUPPATH, mode=WriteMode('overwrite'))
+        except ApiError as err:
+            # This checks for the specific error where a user doesn't have
+            # enough Dropbox space quota to upload this file
+            if (err.error.is_path() and
+                    err.error.get_path().reason.is_insufficient_space()):
+                sys.exit("ERROR: Cannot back up; insufficient space.")
+            elif err.user_message_text:
+                print(err.user_message_text)
+                sys.exit()
+            else:
+                print(err)
+                sys.exit()
+if __name__ == '__main__':
+    # Check for an access token
+    if (len(TOKEN) == 0):
+        sys.exit("ERROR: Looks like you didn't add your access token. "
+            "Open up backup-and-restore-example.py in a text editor and "
+            "paste in your token in line 14.")# Create an instance of a Dropbox class, which can make requests to the API.
+    print("Creating a Dropbox object...")
+    dbx = dropbox.Dropbox(TOKEN)
+    try:# Check that the access token is valid
+        dbx.users_get_current_account()
+    except AuthError as err:
+        sys.exit("ERROR: Invalid access token; try re-generating an "
+            "access token from the app console on the web.")
+    backup()    # Create a backup of the current log file
+    print("Data saved to Adam's dropbox by TANKINATOR!!!!!!")
